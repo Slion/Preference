ba0a9422eeb6d1e1b935a4fa6e3c620732f91b9d
Stéphane Lenclud <github@lenclud.com>
2025-08-25 15:23:35 +0200

    Revert "Fix consumer CI build"
    This reverts commit fd3d84e22ba2550663c96df50478ba59ad0f78d1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("com.android.library") version "8.12.1" apply false
 }
-
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    compileSdk = 33
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 33
-    }
-}
